5b7cab4d6142ecdceeb476f0aab4bad1f67999b2
Jean-Luc Watson <jlwatson@stanford.edu>
2014-04-05 00:19:57 -0700

    Added documentation to camera controls

--- Assets/Scripts/Behaviors/CameraControls.cs
+++ Assets/Scripts/Behaviors/CameraControls.cs
@@ -3,8 +3,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// Camera controls. Details the camera modes necessary for close-up interaction with planets and ground units, and space units.
+/// </summary>
 public class CameraControls : MonoBehaviour {
 
+	/// <summary>Camera mode enumeration of those currently in use.</summary>
 	enum CameraMode {
 		/// <summary>Panning in the global space view.</summary>
 		Panning,
@@ -26,14 +30,19 @@ public class CameraControls : MonoBehaviour {
 	public float maximumHeight = 35.0f;
 	public float minRotateRadius = 2.0f;
 
+	//maximum size of the hex tile space map
 	private float maxTileRadius;
 
+	//camera mode
 	private CameraMode mode = CameraMode.Panning;
 	private GameObject inspectTarget;
 
 	private GameObject gameController;
 
 	// Use this for initialization
+	/// <summary>
+	/// Initializes the game controller and defines the outer limits of the gameplay area.
+	/// </summary>
 	void Start () {
 		gameController = GameObject.FindGameObjectWithTag("GameController");
 		maxTileRadius = 0;
@@ -43,40 +52,47 @@ public class CameraControls : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	/// <summary>
+	/// Regulates camera behavior and movement based on the mode in which the camera is in, also providing for transitions between different modes.
+	/// </summary>
 	void Update () {
 
-		if(mode == CameraMode.Panning) {
-			//Set Camera position
-			transform.position = new Vector3(gridPosX + xDisplacement, transform.position.y, gridPosY);
-			Vector3 targetPosition = new Vector3(gridPosX + xDisplacement, cameraHeight, gridPosY);
-			transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
-			transform.LookAt(new Vector3(gridPosX, 0, gridPosY));
+				///<summary> Panning mode: used to view overall hex tile map in space.</summary>
+				if (mode == CameraMode.Panning) {
+						// **Camera settings, initial position, and restrictions **
 
-			Vector3 newPosition = new Vector3(gridPosX, 0, gridPosY);
-			//limit camera to the game board
-			if(newPosition.magnitude > maxTileRadius){
-				newPosition.Normalize();
-				newPosition *= maxTileRadius;
-				gridPosX = newPosition.x;
-				gridPosY = newPosition.z;
-			}
+						//Set Camera position
+						transform.position = new Vector3 (gridPosX + xDisplacement, transform.position.y, gridPosY);
+						Vector3 targetPosition = new Vector3 (gridPosX + xDisplacement, cameraHeight, gridPosY);
+						transform.position = Vector3.Lerp (transform.position, targetPosition, 0.1f);
+						transform.LookAt (new Vector3 (gridPosX, 0, gridPosY));
 
-			cameraHeight = Mathf.Clamp( cameraHeight, minimumHeight, maximumHeight );
+						Vector3 newPosition = new Vector3 (gridPosX, 0, gridPosY);
+						//limit camera to the game board
+						if (newPosition.magnitude > maxTileRadius) {
+								newPosition.Normalize ();
+								newPosition *= maxTileRadius;
+								gridPosX = newPosition.x;
+								gridPosY = newPosition.z;
+						}
 
-			//Get Panning Speed
-			float moveSpeed = baseMoveSpeed*cameraHeight;
-			moveSpeed = Mathf.Max(minimumMoveSpeed, moveSpeed)*Time.deltaTime;
+						cameraHeight = Mathf.Clamp (cameraHeight, minimumHeight, maximumHeight);
 
-			Vector2 move = new Vector2();
+						//Get Panning Speed
+						float moveSpeed = baseMoveSpeed * cameraHeight;
+						moveSpeed = Mathf.Max (minimumMoveSpeed, moveSpeed) * Time.deltaTime;
 
-			#if UNITY_EDITOR
-				//Mouse Based Panning
-				mousePanning(ref move);
+						// ** Camera movement **
 
-				//Mouse-wheel scrolling
-				cameraHeight -= scrollSpeed*Input.GetAxis("Mouse ScrollWheel");
-			#endif
-			#if UNITY_ANDROID
+						Vector2 move = new Vector2 ();
+						#if UNITY_EDITOR
+						//Mouse Based Panning
+						mousePanning (ref move);
+
+						//Mouse-wheel scrolling
+						cameraHeight -= scrollSpeed * Input.GetAxis ("Mouse ScrollWheel");
+						#endif
+						#if UNITY_ANDROID
 				//Touch based panning
 				if(Input.touchCount == 1) {
 					move = -Input.GetTouch(0).deltaPosition;
@@ -88,48 +104,51 @@ public class CameraControls : MonoBehaviour {
 				if(Input.touchCount == 2 ) {
 					cameraHeight /= Pow(touchScroll(),3.0);
 				}
-			#endif
+						#endif
 
-			//Selecting an object
-			if(Input.GetMouseButtonUp(0)){
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit hit;
+						// **Selecting objects while in camera mode**
+						if (Input.GetMouseButtonUp (0)) {
+								Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+								RaycastHit hit;
 
-				if( Physics.Raycast(ray, out hit) ) {
-					//Planets can be inspected from space
-					if(hit.collider.gameObject.tag == "Planet") {
-						inspectTarget = hit.collider.gameObject;
-						mode = CameraMode.Planet;
-						inspectDisplacement = inspectTarget.transform.InverseTransformPoint(transform.position).normalized;
-						rotateRadius = 2.0f;
-					}
-
-					if(hit.collider.gameObject.tag == "SpaceUnit") {
-						mode = CameraMode.Move;
-						previousMode = CameraMode.Panning;
-						moveTarget = hit.collider.gameObject;
-					}
+								if (Physics.Raycast (ray, out hit)) {
+										//Planets can be inspected from space
+										if (hit.collider.gameObject.tag == "Planet") {
+												inspectTarget = hit.collider.gameObject;
+												mode = CameraMode.Planet;
+												inspectDisplacement = inspectTarget.transform.InverseTransformPoint (transform.position).normalized;
+												rotateRadius = 2.0f;
+										}
+										//Inspect space units
+										if (hit.collider.gameObject.tag == "SpaceUnit") {
+												mode = CameraMode.Move;
+												previousMode = CameraMode.Panning;
+												moveTarget = hit.collider.gameObject;
+										}
+								}
+						}
 				}
-			}
-		}
 
-		if( mode == CameraMode.Planet ) {
+				///<summary> Planet mode: camera rotates around the planet to view ground hex map for a specific planet.</summary>
+				if (mode == CameraMode.Planet) {
+						// **Camera settings, initial position, and restrictions **
 
-			transform.position = Vector3.Lerp(transform.position, inspectTarget.transform.TransformPoint(inspectDisplacement*rotateRadius), 0.2f);
+						transform.position = Vector3.Lerp (transform.position, inspectTarget.transform.TransformPoint (inspectDisplacement * rotateRadius), 0.2f);
 
-			Quaternion q = Quaternion.LookRotation(inspectTarget.transform.position - transform.position);
-			transform.rotation = Quaternion.Lerp(transform.rotation, q, 0.5f);
+						Quaternion q = Quaternion.LookRotation (inspectTarget.transform.position - transform.position);
+						transform.rotation = Quaternion.Lerp (transform.rotation, q, 0.5f);
 
-			Vector2 move = new Vector2();
+						// ** Camera movement **
 
-			#if UNITY_EDITOR
-				//Mouse Based Panning
-				mousePanning(ref move);
+						Vector2 move = new Vector2 ();
+						#if UNITY_EDITOR
+						//Mouse Based Panning
+						mousePanning (ref move);
 
-				//Mouse based scrolling
-				rotateRadius -= Input.GetAxis("Mouse ScrollWheel");
-			#endif
-			#if UNITY_ANDROID
+						//Mouse based scrolling
+						rotateRadius -= Input.GetAxis ("Mouse ScrollWheel");
+						#endif
+						#if UNITY_ANDROID
 			//Touch based panning
 			if(Input.touchCount == 1) {
 				move = -Input.GetTouch(0).deltaPosition;
@@ -138,30 +157,35 @@ public class CameraControls : MonoBehaviour {
 			//Touch based scrolling
 			if(Input.touchCount == 2 ) {
 				rotateRadius /= Pow(touchScroll(),3.0);
-			}
-			#endif
+			}f
+						#endif
 
-			inspectDisplacement = Quaternion.AngleAxis(-move.x, Vector3.up)*inspectDisplacement;
-			inspectDisplacement = Quaternion.AngleAxis(-move.y, Vector3.Cross(Vector3.up, inspectDisplacement))*inspectDisplacement;
+						inspectDisplacement = Quaternion.AngleAxis (-move.x, Vector3.up) * inspectDisplacement;
+						inspectDisplacement = Quaternion.AngleAxis (-move.y, Vector3.Cross (Vector3.up, inspectDisplacement)) * inspectDisplacement;
 
-			if(rotateRadius > 3.5f) mode = CameraMode.Returning;
-			if(rotateRadius < minRotateRadius) rotateRadius = minRotateRadius;
+						//scroll away from planet to exit planet camera mode
+						if (rotateRadius > 3.5f)
+								mode = CameraMode.Returning;
+						if (rotateRadius < minRotateRadius)
+								rotateRadius = minRotateRadius;
 
-			//Selecting an object
-			if(Input.GetMouseButtonUp(0)){
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit hit;
-				if(Physics.Raycast(ray, out hit)) {
-					if(hit.collider.gameObject.tag == "GroundUnit") {
-						mode = CameraMode.Move;
-						previousMode = CameraMode.Planet;
-						moveTarget = hit.collider.gameObject;
-					}
+						//Selecting an object
+						if (Input.GetMouseButtonUp (0)) {
+								Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+								RaycastHit hit;
+								if (Physics.Raycast (ray, out hit)) {
+										if (hit.collider.gameObject.tag == "GroundUnit") {
+												mode = CameraMode.Move;
+												previousMode = CameraMode.Planet;
+												moveTarget = hit.collider.gameObject;
+										}
+								}
+						}
 				}
-			}
-		}
 
+		///<summary>Move mode: used to select units; activates the GUI with specific action options</summary>
 		if(mode == CameraMode.Move) {
+
 			Quaternion targetRotation = Quaternion.LookRotation(moveTarget.transform.position - transform.position);
 			transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10.0f*Time.deltaTime);
 
@@ -169,6 +193,7 @@ public class CameraControls : MonoBehaviour {
 			transform.position = Vector3.Lerp(transform.position, targetPosition, 5.0f*Time.deltaTime);
 		}
 
+		///<summary>Returning mode: Used to move back from a planet.</summary>
 		if( mode == CameraMode.Returning ) {
 			Vector3 targetPosition = new Vector3(gridPosX + xDisplacement, cameraHeight, gridPosY);
 			Quaternion targetOrientation = Quaternion.LookRotation(new Vector3(gridPosX, 0, gridPosY) - targetPosition);
@@ -176,21 +201,26 @@ public class CameraControls : MonoBehaviour {
 			transform.position = Vector3.Lerp(transform.position, targetPosition, 5.0f*Time.deltaTime);
 			transform.rotation = Quaternion.Lerp(transform.rotation, targetOrientation, 5.0f*Time.deltaTime);
 
+			//revert to panning mode when arrived at desired position
 			if(Vector3.Distance(transform.position, targetPosition) < 0.1f)
 				mode = CameraMode.Panning;
 		}
 	}
 
+	/// <summary>
+	/// Adds GUI options on top of the camera - allows the user to go back a mode.
+	/// </summary>
 	void OnGUI() {
 		if(mode == CameraMode.Move) {
-			if (GUI.Button(new Rect(10, 10, 150, 100), "Back"))
-				mode = previousMode;
-
-			if(moveTarget.GetComponent<UnitBehavior>().MoveGUI())
+			if (GUI.Button(new Rect(10, 10, 150, 100), "Back") || moveTarget.GetComponent<UnitBehavior>().MoveGUI())
 				mode = previousMode;
 		}
 	}
 
+	/// <summary>
+	/// Pans over the map using the mouse if it's in the margin of the screen.
+	/// </summary>
+	/// <param name="move">Vector that controls direction of movement - values modified/created by function</param>
 	void mousePanning(ref Vector2 move){
 
 		if(Input.mousePosition.x < mouseMargin || Input.mousePosition.x > Screen.width - mouseMargin ||
@@ -212,6 +242,10 @@ public class CameraControls : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Scrolls in and out of the map using touch controls.
+	/// </summary>
+	/// <returns>The scale of the scroll to be used to edit camera height</returns>
 	double touchScroll(){
 
 		Vector2 touchDifference = Input.GetTouch(0).position - Input.GetTouch(1).position;
